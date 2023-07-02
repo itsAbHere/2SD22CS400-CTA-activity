d12e4aacc599de373ab2b9556324de8a1ec80981
itsAbHere <138356326+itsAbHere@users.noreply.github.com>
2023-07-02 20:52:45 +0530

    Update Gui2.java

--- project/Gui2.java
+++ project/Gui2.java
@@ -1,5 +1,4 @@
 package GUI;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -27,28 +26,32 @@ public class Gui2 extends JFrame {
         setLayout(new GridLayout(6, 2, 10, 10));
         getContentPane().setBackground(new Color(255, 204, 153)); // Set background color to light orange
 
-        add(new JLabel("IA-1 Marks:"));
+        Font labelFont = new Font("Montserrat", Font.PLAIN, 20); // Create a Font object for Montserrat with size 20
+        Font buttonFont = new Font("Arial", Font.PLAIN, 20); // Create a Font object for Arial with size 20
+
+        add(createLabel("IA-1 Marks:", labelFont));
         ia1TextField = new JTextField();
         add(ia1TextField);
 
-        add(new JLabel("IA-2 Marks:"));
+        add(createLabel("IA-2 Marks:", labelFont));
         ia2TextField = new JTextField();
         add(ia2TextField);
 
-        add(new JLabel("IA-3 Marks:"));
+        add(createLabel("IA-3 Marks:", labelFont));
         ia3TextField = new JTextField();
         add(ia3TextField);
 
-        add(new JLabel("CTA Marks:"));
+        add(createLabel("CTA Marks:", labelFont));
         ctaTextField = new JTextField();
         add(ctaTextField);
 
-        add(new JLabel("SEE Marks:"));
+        add(createLabel("SEE Marks:", labelFont));
         seeTextField = new JTextField();
         add(seeTextField);
 
         calculateButton = new JButton("Calculate");
         calculateButton.setBackground(Color.WHITE); // Set button color to white
+        calculateButton.setFont(buttonFont); // Set the font for the button text
         add(calculateButton);
 
         calculateButton.addActionListener(new ActionListener() {
@@ -61,55 +64,55 @@ public class Gui2 extends JFrame {
         setVisible(true);
     }
 
+    private JLabel createLabel(String text, Font font) {
+        JLabel label = new JLabel(text);
+        label.setFont(font);
+        return label;
+    }
+
     private void calculateGrade() {
-        try {
-            int ia1 = Integer.parseInt(ia1TextField.getText());
-            int ia2 = Integer.parseInt(ia2TextField.getText());
-            int ia3 = Integer.parseInt(ia3TextField.getText());
-            int cta = Integer.parseInt(ctaTextField.getText());
-            int see = Integer.parseInt(seeTextField.getText());
+        int ia1Marks = Integer.parseInt(ia1TextField.getText());
+        int ia2Marks = Integer.parseInt(ia2TextField.getText());
+        int ia3Marks = Integer.parseInt(ia3TextField.getText());
+        int ctaMarks = Integer.parseInt(ctaTextField.getText());
+        int seeMarks = Integer.parseInt(seeTextField.getText());
 
-            if (ia1 < 0 || ia1 > 20 || ia2 < 0 || ia2 > 20 || ia3 < 0 || ia3 > 20 ||
-                    cta < 0 || cta > 10 || see < 0 || see > 100) {
-                throw new IllegalArgumentException("Invalid marks entered!");
-            }
+        int cie = ia1Marks + ia2Marks + ia3Marks + ctaMarks;
+        int see = seeMarks;
 
-            int cie = Math.max(ia1, Math.max(ia2, ia3)) + Math.min(ia1, Math.min(ia2, ia3)) + cta;
-
-            if (cie < 20) {
-                JOptionPane.showMessageDialog(null, "Student is detained from taking SEE");
-                return;
-            }
-
-            if (see == 38 || see == 39) {
-                see = 40;
-            }
-
-            int totalMarks = cie + (see / 2);
-
-            String grade;
-            if (totalMarks >= 90 && totalMarks <= 100) {
-                grade = "S";
-            } else if (totalMarks >= 80 && totalMarks < 90) {
-                grade = "A";
-            } else if (totalMarks >= 70 && totalMarks < 80) {
-                grade = "B";
-            } else if (totalMarks >= 60 && totalMarks < 70) {
-                grade = "C";
-            } else if (totalMarks >= 50 && totalMarks < 60) {
-                grade = "D";
-            } else if (totalMarks >= 40 && totalMarks < 50) {
-                grade = "E";
-            } else {
-                grade = "F";
-            }
-
-            JOptionPane.showMessageDialog(null, "Grade: " + grade);
-        } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(null, "Invalid marks entered! Please enter valid numeric values.");
-        } catch (IllegalArgumentException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage());
+        if (cie < 20) {
+            JOptionPane.showMessageDialog(this, "Student is detained from taking SEE");
+            return;
         }
+
+        if (seeMarks == 38 || seeMarks == 39) {
+            see = 40;
+        } else if (seeMarks < 38) {
+            JOptionPane.showMessageDialog(this, "F Grade");
+            return;
+        }
+
+        double totalMarks = (cie) + see / 2.0;
+        int roundedMarks = (int) Math.round(totalMarks);
+
+        String grade;
+        if (roundedMarks >= 90) {
+            grade = "S";
+        } else if (roundedMarks >= 80) {
+            grade = "A";
+        } else if (roundedMarks >= 70) {
+            grade = "B";
+        } else if (roundedMarks >= 60) {
+            grade = "C";
+        } else if (roundedMarks >= 50) {
+            grade = "D";
+        } else if (roundedMarks >= 40) {
+            grade = "E";
+        } else {
+            grade = "F";
+        }
+
+        JOptionPane.showMessageDialog(this, "Grade: " + grade);
     }
 
     public static void main(String[] args) {
@@ -120,4 +123,3 @@ public class Gui2 extends JFrame {
         });
     }
 }
-
